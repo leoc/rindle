bba33e253691a9354d861d3ac49bb779b1066294
leoc <leoc.git@gmail.com>
2011-02-27 17:55:07 +0100

    fixed json load bug and added some collection access methods

--- lib/kindle_fs/collections.rb
+++ lib/kindle_fs/collections.rb
@@ -1,16 +1,69 @@
 require 'rubygems'
 require 'json'
 
+require 'kindle_fs/collection'
+
 module KindleFS
-  class Collections < Hash
+  class Collections
     def initialize kindle_root
-      @collections_file = File.join(kindle_root, 'system', 'collections.json')
+      @collections_file = File.new(File.join(kindle_root, 'system', 'collections.json'))
+      @collections = []
+    end
+    
+    def all
+      @collections.keys.map do |name|
+        name.gsub('@en-US', '')
+      end
+    end
+    
+    def exists?(name)
+      !@collections["#{name}@en-US"].nil?
+    end
+    
+    # creates a new 
+    def create(name)
+      unless exists?(name)
+        @collections["#{name}@en-US"] = {
+          'items' => [],
+          'lastAccess' => Time.now.to_i
+        }
+      end
+      save
     end
 
+    def add_item collection_name, item
+      collection = get(collection_name)
+      collection.add_item(item)
+      save
+    end
+
+    def delete(name)
+      if exists?(name)
+        @collections.remove
+      end
+      save
+    end
+
+    def rename(old, new)
+      if exists?(old)
+        @collections["#{new}@en-US"] = @collections.delete("#{old}@en-US")
+      end
+      save
+    end
+    
+    def get(name)
+      @collections["#{name}@en-US"]
+    end
+    
+    def save
+      JSON.dump(@collections, @collections_file)
+    end
+    
     def load
-      yield 0.0 if block_given?
-      self.merge(JSON.load(@collections_file))
-      yield 1.0 if block_given?
+      @collections = JSON.load(@collections_file)
+      @collections.merge!(@collections) do |name, collection|
+        Collection.new.merge(collection)
+      end
     end
   end
 end
