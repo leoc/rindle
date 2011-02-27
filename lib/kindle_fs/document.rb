29602a19d9ceb0b076ef44efa9f2f437784e1693
leoc <leoc.git@gmail.com>
2011-02-27 18:55:43 +0100

    The KindleFS::Document class serves as model for a normal document (mobi)

---
+++ lib/kindle_fs/document.rb
@@ -0,0 +1,19 @@
+module KindleFS
+  class Document    
+    def initialize index, filename
+      @index = index
+      @filename = filename
+      @collections = Collections.containing(index)
+    end
+    
+    def self.by value
+      if value.is_a?(RegExp)
+        index, name = Index.search(value)
+      else
+        index = Index.index_for(name)
+        index, name = value, Index.name_for(value)
+      end
+      File.new(index, name)
+    end
+  end
+end
