8410471b0bfffecd1df9b899e1da165dbcb43517
leoc <leoc.git@gmail.com>
2011-02-27 19:28:26 +0100

    extended the index dummy with some, yet naive, code

--- lib/kindle_fs/index.rb
+++ lib/kindle_fs/index.rb
@@ -1,16 +1,64 @@
+require "rubygems"
+require "json"
+require "digest/sha1"
+
+def hash(value)
+  Digest::SHA1.hexdigest("/mnt/us/#{value}")
+end
+
 module KindleFS
   class Index < Hash
-    # loads the index information from the system directory found in the root
-    # of the kindles memory. If there is no file to load, it will be created.
-    def initialize(kindle_root)
-      @kindle_root = kindle_root
+    # returns the path for an index
+    def self.name_for(index)
+      @@index[index]
+    end
+    
+    # returns the index for a file path
+    def self.index_for(name)
+      @@index.index(name)
     end
 
-    def load
-      yield 'loading index', 0.0 if block_given?
-      # if index file does not exist create it
-      # if it exists load it
-      # scan through all documents and update the index
+    # search a index, filepath pair via regular expression
+    def self.search(regexp)
+      @@index
+      [ index, name ]
+    end
+    
+    # scans through all the documents on the kindle
+    # adds indices or removes if necessary
+    def self.update
+      Dir["#{@@kindle_root}/documents/*.mobi"].each do |element|
+        puts element
+      end
+    end
+    
+    # adds a filename to the index, while automatically creating the sha1 hash
+    # TODO: handle the amazon kindle book store filenames specially
+    def self.add(filename)
+      index = hash(filename)
+      @@index[index] = filename
+      index
+    end
+
+    # creates the file handler for the kindlefs index file
+    # if the file did not exists, it is created, otherwise loaded and parsed
+    # afterwards the index gets updated and the new new one will be saved
+    def self.load(kindle_root)
+      @@kindle_root = kindle_root
+      if File.exists?(File.join(kindle_root, 'system', 'kindlefs_index.json'))
+        @@index_file = File.new(File.join(kindle_root, 'system', 'kindlefs_index.json'))
+        @@index = JSON.load(@@index_file)
+      else
+        @@index_file = File.new(File.join(kindle_root, 'system', 'kindlefs_index.json'), "w+")
+        @@index = JSON.parse("{}")
+      end
+      update
+      save
+    end
+    
+    # dumps the index onto the kindle system directory
+    def save
+      JSON.dump(@@index, @@index_file)
     end
   end
 end
