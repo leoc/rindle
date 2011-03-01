a9527a99c97340af3284a227f23da9b11775af52
leoc <leoc.git@gmail.com>
2011-03-01 19:40:35 +0100

    removed filesystem mounting code, this file should only be required for use of the library

--- lib/kindle_fs.rb
+++ lib/kindle_fs.rb
@@ -1,29 +1,5 @@
-require 'rubygems'
-require 'fusefs'
-
 require 'kindle_fs/index'
 require 'kindle_fs/collections'
+require 'kindle_fs/collection'
+require 'kindle_fs/document'
 require 'kindle_fs/filesystem'
-
-if ARGV.length < 1
-  puts "usage: kindle_manager <kindle_root> <mount_point>"
-  exit 1
-end
-
-trap("INT") do
-  puts "exiting kindlefs ..."
-  FuseFS.exit
-  FuseFS.unmount
-  exit
-end
-
-kindle_root = ARGV.shift.sub(/#{Regexp.escape(File::SEPARATOR)}$/, '')
-mount_path = ARGV.shift.sub(/#{Regexp.escape(File::SEPARATOR)}$/, '')
-
-KindleFS::Index.load(kindle_root)
-KindleFS::Collections.load(kindle_root)
-
-kindle = KindleFS::Filesystem.new(kindle_root)
-FuseFS.set_root(kindle)
-FuseFS.mount_under mount_path
-FuseFS.run
