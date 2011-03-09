78324782d0fea5145592d63d9e38662c7628f82a
leoc <leoc.git@gmail.com>
2011-03-10 00:09:07 +0100

    began spec'ing rindle/filesystem

--- spec/rindle/filesystem_spec.rb
+++ spec/rindle/filesystem_spec.rb
@@ -1,10 +1,23 @@
 require "spec_helper"
 
 describe Rindle::Filesystem do
-  context 'when mounted' do
-    context 'browsing /collections' do
-      it 'shows the collections loaded'
-      it 'shows files, that weren\'t associated with a collection'
+  before(:all) do
+    Rindle.load(kindle_root)
+    @fs = Rindle::Filesystem.new
+  end
+
+  context '#contents' do
+    it 'lists view options' do
+      list = @fs.contents('/')
+      list.should =~ [ 'collections', 'books', 'pictures' ]
+    end
+    it 'lists collections' do
+      list = @fs.contents('/collections')
+      list.should =~ [ 'collection1', 'collection2', 'amazon books' ]
+    end
+    it 'lists documents in a collection' do
+      list = @fs.contents('/collections/collection1')
+      list.should =~ [ 'A test aswell.mobi', 'Definitely a Test.pdf' ]
     end
   end
 end
