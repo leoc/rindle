4ccfc7b1f3619177910d1eed23d95b82020cc85b
leoc <leoc.git@gmail.com>
2011-04-15 12:48:29 +0200

    added debug output and modified if to case statements

--- lib/rindle/filesystem.rb
+++ lib/rindle/filesystem.rb
@@ -1,8 +1,12 @@
+require 'fusefs'
+
 module Rindle
-  class Filesystem
+  class Filesystem < FuseFS::FuseDir
+
     def contents path
+      puts "contents(#{path})"
       case path
-      when '/'
+      when /^\/$$/
         [ 'collections', 'books', 'pictures' ]
       when /^\/collections$/
         Collection.all.map(&:name)
@@ -10,8 +14,8 @@ module Rindle
         [ 'not_yet_implemented' ]
       when /^\/pictures$/
         [ 'not_yet_implemented' ]
-      when /^\/collections\/([A-Za-z0-9_\-\s'"]+)$/
-        collection = Collection.find(:first, :named => $1)
+      when /^\/collections\/(\._)?([A-Za-z0-9_\-\s'"]+)$/
+        collection = Collection.find(:first, :named => $3)
         collection.documents.map(&:filename)
       else
         []
@@ -22,17 +26,27 @@ module Rindle
     end
     
     def file?(path)
-      if path =~ /^\/(collections|books|pictures)$/
+      puts "file?(#{path})"
+      case path
+      when /^\/(collections|books|pictures)$/
         false
-      elsif path =~ /^\/collections\/([A-Za-z0-9_\-\s'"]+)$/
+      when /^\/collections\/(\._)?([A-Za-z0-9_\-\s'"]+)$/
         false
       else
-        true
+        false
       end
     end
     
     def directory?(path)
-      !file?(path)
+      puts "directory?(#{path})"
+      case path
+      when /^\/[collections|books|pictures]$/
+        true
+      when /^\/collections\/(\._)?([A-Za-z0-9_\-\s'"]+)$/
+        !Collection.first(:named => $3).nil?
+      else
+        false
+      end
     end
     
     def executable?(path)
@@ -40,48 +54,53 @@ module Rindle
     end
 
     def size(path)
+      puts "size(#{path})"
       0
     end
     
     def can_delete?(path)
-#      puts "----- can_delete? #{path}"
+      puts "can_delete?(#{path})"
       true
     end
     
     def can_write?(path)
-#      puts "----- can_write? #{path}"
+      puts "can_write?(#{path})"
       true
     end
 
     def can_mkdir?(path)
-#      puts "----- can_mkdir? #{path}"
+      puts "can_mkdir?(#{path})"
       true
     end
     
     def can_rmdir?(path)
-#      puts "----- can_rmdir? #{path}"
+      puts "can_rmdir?(#{path})"
       true
     end
 
     def touch(path)
-#      puts "----- touch #{path}"
+      puts "touch(#{path})"
     end
     
     def mkdir(path)
-#      puts "----- mkdir #{path}"
+      puts "mkdir(#{path})"
+      false
     end
 
     def rmdir(path)
-#      puts "----- rmdir #{path}"
+      puts "rmdir(#{path})"
+      false
     end
     
     def write_to path, body
-#      puts "write to #{path} - #{body.length}"
+      puts "write_to(#{path}, #{body.length})"
     end
     
     def read_file path
-#      puts "read file #{path}"
+      puts "read_file(#{path})"
       "dummy"
+    rescue Exceptions => e
+      puts e.inspect
     end
   end
 end
